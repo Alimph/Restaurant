fd0aca03e827d63b867dfef387098d55e5e2a8a1
AliMPh <Ali.momeni11.am@gmail.com>
2023-11-27 20:46:07 +0330

    Init

---
+++ SingarRSample/Models/OrderModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace SignalRSmple.Models
+{
+    public static class OrderListModel
+    {
+        public static List<OrderModel> OrderModels { get; set; }
+    }
+    public class OrderModel
+    {
+        public int Code { get; set; }
+        public int FoodId { get; set; }
+        public string CustomerName { get; set; }
+        public bool IsReady { get; set; }
+    }
+
+    public static class OnlineUsers
+    {
+        public static int OnlineUser { get; set; }
+    }
+}
---
+++ SingarRSample/Service/SignalRHub.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRSmple.Models;
+
+namespace SignalRSmple.Service
+{
+    public class SignalRHub : Hub
+    {
+        public async Task InsertOrder(string foodId, string customerName)
+        {
+            OrderListModel.OrderModels = OrderListModel.OrderModels ?? new List<OrderModel>();
+            var lastOrder = OrderListModel.OrderModels.OrderBy(s => s.Code).LastOrDefault();
+
+            var order = new OrderModel
+            {
+                Code = lastOrder != null ? (lastOrder.Code + 1) : 1,
+                CustomerName = customerName,
+                FoodId = int.Parse(foodId),
+            };
+
+            //insert db
+            OrderListModel.OrderModels.Add(order);
+            await Clients.All.SendAsync("GetOrder", order);
+
+        }
+
+        public async Task OrderReady(string code)
+        {
+            await Clients.All.SendAsync("OrderReady", code);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            Clients.All.SendAsync("CommutingUsers", ++OnlineUsers.OnlineUser).Wait();
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            OnlineUsers.OnlineUser--;
+            Clients.All.SendAsync("CommutingUsers", OnlineUsers.OnlineUser).Wait();
+            return base.OnDisconnectedAsync(exception); 
+        }
+    }
+
+}
